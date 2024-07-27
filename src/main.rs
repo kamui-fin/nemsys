c3b156652cd3cd193957dc256a09dad1bbf4ad98
Kamui <fin-kamui@pm.me>
2024-07-26 22:33:54 -0500

    feat: load instruction implementations

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,6 @@ pub struct Cpu {
     registers: registers::Registers,
 }
 
-/*
-* Load/Store Operations
-*
-* These instructions transfer a single byte between memory and one of the registers.
-* Load operations set the negative (N) and zero (Z) flags depending on the value of transferred.
-* Store operations do not affect the flag settings.
-*
-* LDA 	Load Accumulator 	N,Z
-* LDX 	Load X Register 	N,Z
-* LDY 	Load Y Register 	N,Z
-* STA 	Store Accumulator
-* STX 	Store X Register
-* STY 	Store Y Register
-*/
-
 impl Cpu {
     pub fn new() -> Self {
         Self {
@@ -73,6 +58,7 @@ impl Cpu {
     }
 
     // Opcode: $BD
+    // 4 (+1 if page crossed) cycles
     fn lda_absolute_x(&mut self, address: u16) {
         let value = self
             .memory
@@ -81,6 +67,7 @@ impl Cpu {
     }
 
     // Opcode: $B9
+    // 4 (+1 if page crossed) cycles
     fn lda_absolute_y(&mut self, address: u16) {
         let value = self
             .memory
@@ -89,6 +76,7 @@ impl Cpu {
     }
 
     // Opcode: $A1
+    // 6 cycles
     fn lda_indirect_x(&mut self, addr_lower_byte: u8) {
         let value = self
             .memory
@@ -96,12 +84,192 @@ impl Cpu {
         self.lda_immediate(value)
     }
 
-    // Opcode: $B2
+    // Opcode: $B1
+    // 5 (+1 if page crossed) cycles
     fn lda_indirect_y(&mut self, addr_lower_byte: u8) {
         let value = self
             .memory
             .fetch_indirect_y(addr_lower_byte, self.registers.index_y);
         self.lda_immediate(value)
+    }
+
+    /*
+     *
+     * LDX - Load X Register
+     * Loads a byte of memory into the X register setting the zero and negative flags as appropriate.
+     *
+     */
+
+    // Opcode: $A2
+    // 2 cycles
+    fn ldx_immediate(&mut self, value: u8) {
+        self.registers.index_x = value;
+        if (self.registers.index_x == 0) {
+            self.registers.set_zero();
+        }
+        if (self.registers.index_x & 0b1000_0000 == 1) {
+            self.registers.set_neg();
+        }
+    }
+
+    // Opcode: $AE
+    // 4 cycles
+    fn ldx_absolute(&mut self, address: u16) {
+        let value = self.memory.fetch_absolute(address);
+        self.ldx_immediate(value)
+    }
+
+    // Opcode: $BE
+    // 4 (+1 if page crossed) cycles
+    fn ldx_absolute_y(&mut self, address: u16) {
+        let value = self
+            .memory
+            .fetch_absolute_x(address, self.registers.index_y);
+        self.ldx_immediate(value)
+    }
+
+    // Opcode: $A6
+    // 3 cycles
+    fn ldx_zero_page(&mut self, addr_lower_byte: u8) {
+        let value = self.memory.fetch_zero_page(addr_lower_byte);
+        self.ldx_immediate(value)
+    }
+
+    // Opcode: $B6
+    // 4 cycles
+    fn ldx_zero_page_y(&mut self, addr_lower_byte: u8) {
+        let value = self
+            .memory
+            .fetch_zero_page_x(addr_lower_byte, self.registers.index_y);
+        self.ldx_immediate(value)
+    }
+
+    /*
+     *
+     * LDY - Load Y Register
+     * Loads a byte of memory into the Y register setting the zero and negative flags as appropriate.
+     *
+     */
+
+    // Opcode: $A0
+    // 2 cycles
+    fn ldy_immediate(&mut self, value: u8) {
+        self.registers.index_y = value;
+        if (self.registers.index_y == 0) {
+            self.registers.set_zero();
+        }
+        if (self.registers.index_y & 0b1000_0000 == 1) {
+            self.registers.set_neg();
+        }
+    }
+
+    // Opcode: $AC
+    // 4 cycles
+    fn ldy_absolute(&mut self, address: u16) {
+        let value = self.memory.fetch_absolute(address);
+        self.ldy_immediate(value)
+    }
+
+    // Opcode: $BC
+    // 4 (+1 if page crossed) cycles
+    fn ldy_absolute_x(&mut self, address: u16) {
+        let value = self
+            .memory
+            .fetch_absolute_x(address, self.registers.index_x);
+        self.ldy_immediate(value)
+    }
+
+    // Opcode: $A4
+    // 3 cycles
+    fn ldy_zero_page(&mut self, addr_lower_byte: u8) {
+        let value = self.memory.fetch_zero_page(addr_lower_byte);
+        self.ldy_immediate(value)
+    }
+
+    // Opcode: $B4
+    // 4 cycles
+    fn ldy_zero_page_x(&mut self, addr_lower_byte: u8) {
+        let value = self
+            .memory
+            .fetch_zero_page_x(addr_lower_byte, self.registers.index_x);
+        self.ldy_immediate(value)
+    }
+
+    /*
+     *   TAX - Transfer accumulator to X
+     *   Copies the current contents of the accumulator into the X register and sets the zero and negative flags as appropriate.
+     *
+     *   Opcode: $AA
+     *   Cycles: 2
+     */
+    fn tax(&mut self) {
+        self.ldx_immediate(self.registers.accumulator);
+    }
+
+    /*
+     *   TAY - Transfer Accumulator to Y
+     *   Copies the current contents of the accumulator into the Y register and sets the zero and negative flags as appropriate.
+     *
+     *   Opcode: $A8
+     *   Cycles: 2
+     */
+    fn tay(&mut self) {
+        self.ldy_immediate(self.registers.accumulator);
+    }
+
+    /*
+     *   TSX - Transfer Stack Pointer to X
+     *   Copies the current contents of the stack register into the X register and sets the zero and negative flags as appropriate.
+     *
+     *   Opcode: $BA
+     *   Cycles: 2
+     */
+    fn tsx(&mut self) {
+        self.ldx_immediate(self.registers.stack_pointer)
+    }
+
+    /*
+     *   TXA - Transfer X to accumulator
+     *   Copies the current contents of the X register into the accumulator and sets the zero and negative flags as appropriate.
+     *
+     *   Opcode: $8A
+     *   Cycles: 2
+     */
+    fn txa(&mut self) {
+        self.lda_immediate(self.registers.index_x)
+    }
+
+    /*
+     *   TXS - Transfer X to Stack Pointer
+     *   Copies the current contents of the X register into the stack register.
+     *
+     *   Opcode: $9A
+     *   Cycles: 2
+     */
+    fn txs(&mut self) {
+        self.registers.stack_pointer = self.registers.index_x;
+    }
+
+    /*
+     *   TYA - Transfer Y to Accumulator
+     *   Copies the current contents of the Y register into the accumulator and sets the zero and negative flags as appropriate.
+     *
+     *   Opcode: $98
+     *   Cycles: 2
+     */
+    fn tya(&mut self) {
+        self.lda_immediate(self.registers.index_y)
+    }
+
+    /*
+     *   CLC - Clear Carry Flag
+     *   Set the carry flag to zero.
+     *
+     *   Opcode: $18
+     *   Cycles: 2
+     */
+    fn clc(&mut self) {
+        self.registers.set_carry(0);
     }
 }
 
